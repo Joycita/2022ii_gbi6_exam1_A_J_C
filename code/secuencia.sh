c0861bbc40e7babb4c4c68b8099bce7474904e5d
Joycita <117690379+Joycita@users.noreply.github.com>
2022-11-25 21:00:23 -0500

    Joyce añadio secuencia a code

--- code/secuencia.sh
+++ code/secuencia.sh
@@ -1,1 +1,6 @@
 ## Datos de secuencia 
+## Cuente la cantidad de registros de cada tipo de péptido [secuence] y guarde el resultado en amp.txt 
+
+cat ../data/grampa.csv  | cut -d "," -f 3| tail -n +2 | sort | uniq -c>../data/amp.txt 
+
+
