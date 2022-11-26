663b458335c93b347cf2c2dca8ba663b8f0785fc
Joycita <117690379+Joycita@users.noreply.github.com>
2022-11-26 08:03:40 -0500

    Andrade Joyce ha determinado los Péptidos con el Dominio WGS

--- code/secuencia.sh
+++ code/secuencia.sh
@@ -2,19 +2,19 @@
 ## Cuente la cantidad de registros de cada tipo de péptido [secuence] y guarde el resultado en amp.txt
 cd ../data/
 
-tail -n +2 grampa.csv | cut -d "," -f 4 | uniq -c | sort -n >amp.txt
+tail -n +2 grampa.csv | cut -d "," -f 4 | uniq -c | sort -n
 
 ## Calcule la longitud de los péptidos que tienen el dominio “WGS” y guarde los registros y su conteo en el “wgs.csv”
 cd ../data/
 
 
-PeptidosconeldominioWGS=`tail -n +1 grampa.csv | cut -d "," -f 4 | grep WGS | uniq`>wgs.csv
+PeptidosconeldominioWGS=`tail -n +1 grampa.csv | cut -d "," -f 4 | grep WGS | uniq`
 
 for z in $PeptidosconeldominioWGS 
 do
   LongituddelosPeptidos=`echo ${#z} | sort -n`
   echo  "Longitud:" $LongituddelosPeptidos "Peptido" $z
-done >wgs.csv
+done
 
 
 ##Envíe los cambios al repositorio principal con la leyenda “Apellido Nombre ha determinado los péptidos con dominio WGS”.
